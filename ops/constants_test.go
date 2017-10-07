30ff12586196dc052893025afa2a368acef63be8
Benjamin Campbell <i@benjic.me>
2017-10-07 13:36:34 -0600

    Simplify constant tests

--- ops/constants_test.go
+++ ops/constants_test.go
@@ -5,99 +5,93 @@ import (
 	"testing"
 )
 
-func Test_opFalse(t *testing.T) {
+func TestConstantsOps(t *testing.T) {
 	type args struct {
-		c *context
+		context *context
 	}
+	type want struct {
+		stack *stack
+		alt   *stack
+		err   error
+	}
+	type test struct {
+		name string
+		args args
+		want want
+	}
+
 	tests := []struct {
-		name    string
-		args    args
-		want    *stack
-		wantErr bool
+		name  string
+		op    Op
+		tests []test
 	}{
 		{
-			"pushes false",
-			args{emptyContext()},
-			&stack{[]byte{0x00, 0x00, 0x0, 0x00}},
-			false,
+			"opFalse",
+			opFalse,
+			[]test{
+				{
+					"pushes false",
+					args{emptyContext()},
+					want{
+						&stack{[]byte{0x00, 0x00, 0x0, 0x00}},
+						&stack{},
+						nil,
+					},
+				},
+			},
 		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			err := opFalse(tt.args.c)
-
-			if (err != nil) != tt.wantErr {
-				t.Errorf("opFalse() error = %v, wantErr %v", err, tt.wantErr)
-			}
-
-			if !reflect.DeepEqual(tt.args.c.stack, tt.want) {
-				t.Errorf("Want %+v; Got %+v", tt.args.c, tt.want)
-			}
-		})
-	}
-}
-
-func Test_op1Negate(t *testing.T) {
-	type args struct {
-		c *context
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    *stack
-		wantErr bool
-	}{
 		{
-			"pushes negative 1",
-			args{emptyContext()},
-			&stack{[]byte{0x40, 0x00, 0x00, 0x01}},
-			false,
+			"op1Negate",
+			op1Negate,
+			[]test{
+				{
+					"pushes negative 1",
+					args{emptyContext()},
+					want{
+						&stack{[]byte{0x40, 0x00, 0x00, 0x01}},
+						&stack{},
+						nil,
+					},
+				},
+			},
 		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			err := op1Negate(tt.args.c)
-
-			if (err != nil) != tt.wantErr {
-				t.Errorf("op1Negate() error = %v, wantErr %v", err, tt.wantErr)
-			}
-
-			if !reflect.DeepEqual(tt.args.c.stack, tt.want) {
-				t.Errorf("Want %+v; Got %+v", tt.args.c, tt.want)
-			}
-		})
-	}
-}
-
-func Test_opTrue(t *testing.T) {
-	type args struct {
-		c *context
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    *stack
-		wantErr bool
-	}{
 		{
-			"pushes 1",
-			args{emptyContext()},
-			&stack{[]byte{0x00, 0x00, 0x00, 0x01}},
-			false,
+			"opTrue",
+			opTrue,
+			[]test{
+				//
+				{
+					"pushes 1",
+					args{emptyContext()},
+					want{
+						&stack{[]byte{0x00, 0x00, 0x00, 0x01}},
+						&stack{},
+						nil,
+					},
+				},
+			},
 		},
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			err := opTrue(tt.args.c)
 
-			if (err != nil) != tt.wantErr {
-				t.Errorf("opTrue() error = %v, wantErr %v", err, tt.wantErr)
-			}
+	for _, opTest := range tests {
+		for _, test := range opTest.tests {
 
-			if !reflect.DeepEqual(tt.args.c.stack, tt.want) {
-				t.Errorf("Want %+v; Got %+v", tt.args.c, tt.want)
-			}
-		})
+			t.Run(opTest.name+" "+test.name, func(t *testing.T) {
+				err := opTest.op(test.args.context)
+				if err != test.want.err {
+					t.Errorf("%s() error = %v, want err %v", opTest.name, err, test.want.err)
+				}
+
+				if !reflect.DeepEqual(test.want.stack, test.args.context.stack) {
+					t.Errorf("want %v; got %v", test.want.stack, test.args.context.stack)
+				}
+
+				if !reflect.DeepEqual(test.want.alt, test.args.context.alt) {
+					t.Errorf("want %v; got %v", test.want.alt, test.args.context.alt)
+				}
+			})
+
+		}
 	}
 }
 
