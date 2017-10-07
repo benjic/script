ae2b9107f794edc1397cd9d4a4699b0c2d22ee75
Benjamin Campbell <i@benjic.me>
2017-10-07 13:23:01 -0600

    Simplify stack tests

--- ops/stack_test.go
+++ ops/stack_test.go
@@ -5,303 +5,215 @@ import (
 	"testing"
 )
 
-func Test_opToAltStack(t *testing.T) {
+func TestStackOps(t *testing.T) {
 	type args struct {
-		c *context
+		context *context
 	}
 	type want struct {
 		stack *stack
 		alt   *stack
 		err   error
 	}
-	tests := []struct {
+	type test struct {
 		name string
 		args args
 		want want
+	}
+
+	tests := []struct {
+		name  string
+		op    Op
+		tests []test
 	}{
 		{
-			"empty stack",
-			args{contextWithStackAndAlt(&stack{}, &stack{[]byte{0x00}})},
-			want{
-				&stack{},
-				&stack{[]byte{0x00}},
-				ErrInvalidStackOperation,
+			"opToAltStack",
+			opToAltStack,
+			[]test{
+				{
+					"empty stack",
+					args{contextWithStackAndAlt(&stack{}, &stack{[]byte{0x00}})},
+					want{
+						&stack{},
+						&stack{[]byte{0x00}},
+						ErrInvalidStackOperation,
+					},
+				},
+				{
+					"simple",
+					args{contextWithStackAndAlt(&stack{[]byte{0x00}, []byte{0xff}}, &stack{[]byte{0x00}})},
+					want{
+						&stack{[]byte{0x00}},
+						&stack{[]byte{0x00}, []byte{0xff}},
+						nil,
+					},
+				},
 			},
 		},
 		{
-			"simple",
-			args{contextWithStackAndAlt(&stack{[]byte{0x00}, []byte{0xff}}, &stack{[]byte{0x00}})},
-			want{
-				&stack{[]byte{0x00}},
-				&stack{[]byte{0x00}, []byte{0xff}},
-				nil,
+			"opFromAltStack",
+			opFromAltStack,
+			[]test{
+				{
+					"simple",
+					args{contextWithStackAndAlt(&stack{[]byte{0x00}}, &stack{[]byte{0x00}, []byte{0xff}})},
+					want{
+						&stack{[]byte{0x00}, []byte{0xff}},
+						&stack{[]byte{0x00}},
+						nil,
+					},
+				},
+				{
+					"empty stack",
+					args{contextWithStackAndAlt(&stack{[]byte{0x00}}, &stack{})},
+					want{
+						&stack{[]byte{0x00}},
+						&stack{},
+						ErrInvalidStackOperation,
+					},
+				},
+			},
+		},
+		{
+			"opIfDup",
+			opIfDup,
+			[]test{
+				{
+					"non zero top stack",
+					args{contextWithStack(&stack{[]byte{0x00, 0x00, 0x00, 0x01}})},
+					want{
+						&stack{[]byte{0x00, 0x00, 0x00, 0x01}, []byte{0x00, 0x00, 0x00, 0x01}},
+						&stack{},
+						nil,
+					},
+				},
+				{
+					"zero top stack",
+					args{contextWithStack(&stack{[]byte{0x00, 0x00, 0x00, 0x00}})},
+					want{
+						&stack{[]byte{0x00, 0x00, 0x00, 0x00}},
+						&stack{},
+						nil,
+					},
+				},
+			},
+		},
+		{
+			"opDepth",
+			opDepth,
+			[]test{
+				{
+					"empty stack",
+					args{contextWithStack(&stack{})},
+					want{
+						&stack{[]byte{0x00, 0x00, 0x00, 0x00}},
+						&stack{},
+						nil,
+					},
+				},
+				{
+					"depth 1",
+					args{contextWithStack(&stack{[]byte{0x00}})},
+					want{
+						&stack{[]byte{0x00}, []byte{0x01, 0x00, 0x00, 0x00}},
+						&stack{},
+						nil,
+					},
+				},
+				//
+			},
+		},
+		{
+			"opDrop",
+			opDrop,
+			[]test{
+				{
+					"empty stack",
+					args{contextWithStack(&stack{})},
+					want{
+						&stack{},
+						&stack{},
+						ErrInvalidStackOperation,
+					},
+				},
+				{
+					"single value",
+					args{contextWithStack(&stack{[]byte{0x00}})},
+					want{
+						&stack{},
+						&stack{},
+						nil,
+					},
+				},
+			},
+		},
+		{
+			"opDup",
+			opDup,
+			[]test{
+				{
+					"simple",
+					args{contextWithStack(&stack{{0x1}})},
+					want{
+						&stack{{0x1}, {0x1}},
+						&stack{},
+						nil,
+					},
+				},
+				{
+					"empty stack",
+					args{contextWithStack(&stack{})},
+					want{
+						&stack{},
+						&stack{},
+						ErrInvalidStackOperation,
+					},
+				},
+			},
+		},
+		{
+			"opOver",
+			opOver,
+			[]test{
+				{
+					"simple",
+					args{contextWithStack(&stack{{0x1}, {0x2}})},
+					want{
+						&stack{{0x1}, {0x2}, {0x1}},
+						&stack{},
+						nil,
+					},
+				},
+				{
+					"too small stack",
+					args{contextWithStack(&stack{{0x1}})},
+					want{
+						&stack{{0x1}},
+						&stack{},
+						ErrInvalidStackOperation,
+					},
+				},
 			},
 		},
 	}
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			err := opToAltStack(tt.args.c)
-			if err != tt.want.err {
-				t.Errorf("opToAltStack() error = %v, want err %v", err, tt.want.err)
-			}
+	for _, opTest := range tests {
+		for _, test := range opTest.tests {
 
-			if !reflect.DeepEqual(tt.want.stack, tt.args.c.stack) {
-				t.Errorf("want %v; got %v", tt.want.stack, tt.args.c.stack)
-			}
+			t.Run(opTest.name+" "+test.name, func(t *testing.T) {
+				err := opTest.op(test.args.context)
+				if err != test.want.err {
+					t.Errorf("%s() error = %v, want err %v", opTest.name, err, test.want.err)
+				}
 
-			if !reflect.DeepEqual(tt.want.alt, tt.args.c.alt) {
-				t.Errorf("want %v; got %v", tt.want.alt, tt.args.c.alt)
-			}
-		})
-	}
-}
+				if !reflect.DeepEqual(test.want.stack, test.args.context.stack) {
+					t.Errorf("want %v; got %v", test.want.stack, test.args.context.stack)
+				}
 
-func Test_opFromAltStack(t *testing.T) {
-	type args struct {
-		c *context
-	}
-	type want struct {
-		stack *stack
-		alt   *stack
-		err   error
-	}
-	tests := []struct {
-		name string
-		args args
-		want want
-	}{
-		{
-			"simple",
-			args{contextWithStackAndAlt(&stack{[]byte{0x00}}, &stack{[]byte{0x00}, []byte{0xff}})},
-			want{
-				&stack{[]byte{0x00}, []byte{0xff}},
-				&stack{[]byte{0x00}},
-				nil,
-			},
-		},
-		{
-			"empty stack",
-			args{contextWithStackAndAlt(&stack{[]byte{0x00}}, &stack{})},
-			want{
-				&stack{[]byte{0x00}},
-				&stack{},
-				ErrInvalidStackOperation,
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			err := opFromAltStack(tt.args.c)
-			if err != tt.want.err {
-				t.Errorf("opFromAltStack() error = %v, want err %v", err, tt.want.err)
-			}
+				if !reflect.DeepEqual(test.want.alt, test.args.context.alt) {
+					t.Errorf("want %v; got %v", test.want.alt, test.args.context.alt)
+				}
+			})
 
-			if !reflect.DeepEqual(tt.want.stack, tt.args.c.stack) {
-				t.Errorf("want %v; got %v", tt.want.stack, tt.args.c.stack)
-			}
-
-			if !reflect.DeepEqual(tt.want.alt, tt.args.c.alt) {
-				t.Errorf("want %v; got %v", tt.want.alt, tt.args.c.alt)
-			}
-		})
-	}
-}
-
-func Test_opIfDup(t *testing.T) {
-	type args struct {
-		c *context
-	}
-	type want struct {
-		err   bool
-		stack *stack
-	}
-	tests := []struct {
-		name string
-		args args
-		want want
-	}{
-		{
-			"non zero top stack",
-			args{contextWithStack(&stack{[]byte{0x00, 0x00, 0x00, 0x01}})},
-			want{false, &stack{[]byte{0x00, 0x00, 0x00, 0x01}, []byte{0x00, 0x00, 0x00, 0x01}}},
-		},
-		{
-			"zero top stack",
-			args{contextWithStack(&stack{[]byte{0x00, 0x00, 0x00, 0x00}})},
-			want{false, &stack{[]byte{0x00, 0x00, 0x00, 0x00}}},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			err := opIfDup(tt.args.c)
-			if (err != nil) != tt.want.err {
-				t.Errorf("opFromAltStack() error = %v, want err %v", err, tt.want.err)
-			}
-
-			if !reflect.DeepEqual(tt.want.stack, tt.args.c.stack) {
-				t.Errorf("want %v; got %v", tt.want.stack, tt.args.c.stack)
-			}
-		})
-	}
-}
-
-func Test_opDepth(t *testing.T) {
-	type args struct {
-		c *context
-	}
-	type want struct {
-		err   bool
-		stack *stack
-		alt   *stack
-	}
-	tests := []struct {
-		name string
-		args args
-		want want
-	}{
-		{
-			"empty stack",
-			args{contextWithStack(&stack{})},
-			want{false, &stack{[]byte{0x00, 0x00, 0x00, 0x00}}, &stack{}},
-		},
-		{
-			"depth 1",
-			args{contextWithStack(&stack{[]byte{0x00}})},
-			want{
-				false,
-				&stack{[]byte{0x00}, []byte{0x01, 0x00, 0x00, 0x00}},
-				&stack{},
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			err := opDepth(tt.args.c)
-			if (err != nil) != tt.want.err {
-				t.Errorf("opFromAltStack() error = %v, want err %v", err, tt.want.err)
-			}
-
-			if !reflect.DeepEqual(tt.want.stack, tt.args.c.stack) {
-				t.Errorf("want %v; got %v", tt.want.stack, tt.args.c.stack)
-			}
-
-			if !reflect.DeepEqual(tt.want.alt, tt.args.c.alt) {
-				t.Errorf("want %v; got %v", tt.want.alt, tt.args.c.alt)
-			}
-		})
-	}
-}
-
-func Test_opDrop(t *testing.T) {
-	type args struct {
-		c *context
-	}
-	type want struct {
-		err   error
-		stack *stack
-	}
-	tests := []struct {
-		name string
-		args args
-		want want
-	}{
-		{
-			"empty stack",
-			args{contextWithStack(&stack{})},
-			want{ErrInvalidStackOperation, &stack{}},
-		},
-		{
-			"single value",
-			args{contextWithStack(&stack{[]byte{0x00}})},
-			want{nil, &stack{}},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			err := opDrop(tt.args.c)
-			if err != tt.want.err {
-				t.Errorf("opFromAltStack() error = %v, want err %v", err, tt.want.err)
-			}
-
-			if !reflect.DeepEqual(tt.want.stack, tt.args.c.stack) {
-				t.Errorf("want %v; got %v", tt.want.stack, tt.args.c.stack)
-			}
-		})
-	}
-}
-
-func Test_opDup(t *testing.T) {
-	type args struct {
-		c *context
-	}
-	type want struct {
-		err   error
-		stack *stack
-	}
-	tests := []struct {
-		name string
-		args args
-		want want
-	}{
-		{
-			"simple",
-			args{contextWithStack(&stack{{0x1}})},
-			want{nil, &stack{{0x1}, {0x1}}},
-		},
-		{
-			"empty stack",
-			args{contextWithStack(&stack{})},
-			want{ErrInvalidStackOperation, &stack{}},
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if err := opDup(tt.args.c); err != tt.want.err {
-				t.Errorf("opDup() error = %v, wantErr %v", err, tt.want.err)
-			}
-
-			if !reflect.DeepEqual(tt.want.stack, tt.args.c.stack) {
-				t.Errorf("want %v; got %v", tt.want.stack, tt.args.c.stack)
-			}
-		})
-	}
-}
-
-func Test_opOver(t *testing.T) {
-	type args struct {
-		c *context
-	}
-	type want struct {
-		err   error
-		stack *stack
-	}
-	tests := []struct {
-		name string
-		args args
-		want want
-	}{
-		{
-			"simple",
-			args{contextWithStack(&stack{{0x1}, {0x2}})},
-			want{nil, &stack{{0x1}, {0x2}, {0x1}}},
-		},
-		{
-			"too small stack",
-			args{contextWithStack(&stack{{0x1}})},
-			want{ErrInvalidStackOperation, &stack{{0x1}}},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if err := opOver(tt.args.c); err != tt.want.err {
-				t.Errorf("opOver() error = %v, wantErr %v", err, tt.want.err)
-			}
-			if !reflect.DeepEqual(tt.want.stack, tt.args.c.stack) {
-				t.Errorf("want %v; got %v", tt.want.stack, tt.args.c.stack)
-			}
-
-		})
+		}
 	}
 }
